e555e6df6bb861cfeb72f98a26c58bea67e8c2e2
Shweta <shweta@antiersolutions.com>
2026-01-19 13:46:58 +0530

    fix: issue of  vite cofig file

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    host: '0.0.0.0',
     port: 5173,
     allowedHosts: [
       "nftmarkateplace-frontend.onrender.com"
     ]
   },
   preview: {
-    host: true,
+    host: '0.0.0.0',
     port: 5173,
     allowedHosts: [
       "nftmarkateplace-frontend.onrender.com"
